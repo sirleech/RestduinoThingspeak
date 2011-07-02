c11494bfca51f9b6b6c800c8cbc5f2f6d3487922
Sirleech-turnkey <root@ausbots.dyndns.org>
2011-07-02 21:33:35 +1000

    thingspeak log to ch. 826

--- log.py
+++ log.py
@@ -6,9 +6,17 @@ try:
 	arduino.request("GET","/a0")
 	response = arduino.getresponse()
 	print "Arduino response:" , response.status, response.reason
-	data = response.read()
-	print data
+	a0 = response.read()
+	print "a0:", a0
+
+	params = urllib.urlencode({'field1': a0,'key':'6Z6I1R4JT1YK6L9F'})
+	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+	conn = httplib.HTTPConnection("api.thingspeak.com:80")
+	conn.request("POST", "/update", params, headers)
+	ts_response = conn.getresponse()
+	print "Thingspeak Response:", ts_response.status, ts_response.reason
+	conn.close
 except:
-	print "Arduino connection failed!"
+	print "connection failed!"
 
 
