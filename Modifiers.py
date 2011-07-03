1ad7f593b83ba66ecc8ea55795c89c9fa620d30c
Sirleech-turnkey <root@ausbots.dyndns.org>
2011-07-03 19:18:21 +1000

    thermistor calculations

--- Modifiers.py
+++ Modifiers.py
@@ -1,6 +1,14 @@
 # Define modifiers for your raw sensor values
 # e.g. Thermistor calculations
 
+import math
+
 def modify(rawValue):
-        return float(rawValue)
+        a0 = float(rawValue)
+	B = 3975
+        resistance = (1024-a0)*10000/a0
+        print resistance
+        tempC = 1/(math.log(resistance/10000)/B+1/298.15)-273.15
+	roundTempC = round(tempC,2)
+	return roundTempC
 
