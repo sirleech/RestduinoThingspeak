7abced3e7ba13d9ca88bc719171a14807afcee85
Chris Lee <chris@australianrobotics.com.au>
2011-07-05 14:17:33 +1000

    jsonp tests

--- webui/jsonp.php
+++ webui/jsonp.php
@@ -3,7 +3,7 @@
 // put this on a remote server, different domain then your web client
 
 $analog = rand(300,500);
-$jsonData = "{\"a0\":\"$analog.\"}";
+$jsonData = "{\"a0\":\"$analog\"}";
 echo $_GET['callback'] . '(' . $jsonData . ');';
 // prints: jsonp1232617941775({"symbol" : "IBM", "price" : "91.42"});
 
